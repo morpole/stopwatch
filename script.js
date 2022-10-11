48dda8e8532a159145896d9f9bd3b7dc5579297a
morpole <morpole@yahoo.com>
2022-10-11 11:33:46 +0100

    added more javascript

---
+++ script.js
@@ -0,0 +1,29 @@
+// Variables for buttons
+
+const startStopBtn = document.querySelector("#startStopBtn");
+const resetBtn = document.querySelector("#resetBtn");
+
+// Variables for time values
+
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+function stopWatch() {
+  seconds++;
+
+  if (seconds / 60 === 1) {
+    seconds = 0;
+    minutes++;
+
+    if (minutes / 60 === 1) {
+      minutes = 0;
+      hours++
+    }
+  }
+
+  let displayTimer = (document.getElementById("timer").innerText =
+    hours + ":" + minutes + ":" + seconds);
+}
+
+window.setInterval(stopWatch, 1000);
