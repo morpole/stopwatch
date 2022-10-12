d97d30cf41ff42baaf350748526b4187e02984cb
morpole <morpole@yahoo.com>
2022-10-12 11:01:42 +0100

    added functionality

--- script.js
+++ script.js
@@ -9,6 +9,19 @@ let seconds = 0;
 let minutes = 0;
 let hours = 0;
 
+//Variables for leading zero//
+
+let leadingSeconds = 0;
+let leadingMinutes = 0;
+let leadingHours = 0;
+
+//Variables for set interval & timerstatus
+
+let timerInterval = null;
+let timerStatus = "stopped";
+
+//Stop Watch Function
+
 function stopWatch() {
   seconds++;
 
@@ -18,12 +31,57 @@ function stopWatch() {
 
     if (minutes / 60 === 1) {
       minutes = 0;
-      hours++
+      hours++;
     }
   }
 
+  if (seconds < 10) {
+    leadingSeconds = "0" + seconds.toString();
+  } else {
+    leadingSeconds = seconds;
+  }
+
+  if (minutes < 10) {
+    leadingMinutes = "0" + minutes.toString();
+  } else {
+    leadingMinutes = minutes;
+  }
+
+  if (hours < 10) {
+    leadingHours = "0" + hours.toString();
+  } else {
+    leadingHours = hours;
+  }
+
   let displayTimer = (document.getElementById("timer").innerText =
-    hours + ":" + minutes + ":" + seconds);
+    leadingHours + ":" + leadingMinutes + ":" + leadingSeconds);
 }
 
-window.setInterval(stopWatch, 1000);
+//Starting-Pausing the Timer
+
+startStopBtn.addEventListener("click", function () {
+  if (timerStatus === "stopped") {
+    timerInterval = window.setInterval(stopWatch, 1000);
+    document.getElementById(
+      "startStopBtn"
+    ).innerHTML = `<i class="fa-solid fa-pause" id="pause"></i>`;
+    timerStatus = "started";
+  } else {
+    window.clearInterval(timerInterval);
+    document.getElementById(
+      "startStopBtn"
+    ).innerHTML = `<i class="fa-solid fa-play" id="play"></i>`;
+    timerStatus = "stopped";
+  }
+});
+
+//Resetting the Timer
+
+resetBtn.addEventListener("click", function () {
+  window.clearInterval(timerInterval);
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
+
+  document.getElementById("timer").innerHTML = "00:00:00";
+});
